ecca4f0d92a919b80a2a6b3d4e2fc1e1b04d535c
Kyle Parsons <23gdsm@gmail.com>
2018-06-15 16:48:38 -0700

    final commit before 5

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,44 @@ function Pizza (size, toppings) {
 
 Pizza.prototype.checkPrice = function () {
 return this.size + this.toppings;
-};
+}
 
 
-Pizza.protoype.sizeSelect = function () {
-  if (userSize = "1") {
+Pizza.prototype.sizeSelect = function () {
+
+
+  if (userSize === "1") {
     this.size = 8
-  }else if (userSize = "2") {
+  }else if (userSize === "2") {
     this.size = 10
-  }else  {
+  }else {
     this.size = 12
-};
+  }
+}
 
-Pizza.prototype.toppingsSelect= function () {
+
+Pizza.prototype.toppingsSelect = function () {
 
   var toppingsTotal = userToppings1 + userToppings2 + userToppings3;
-  if (toppingsTotal = 1) {
+  if (toppingsTotal === "1") {
     this.toppings = 1.75
-  }else if (toppingsTotal = 2) {
+  }else if (toppingsTotal === "2") {
     this.toppings = 3.50
-  }else if (toppingsTotals = 3) {
+  }else if (toppingsTotals === "3") {
     this.toppings = 5.25
-  }else
+  }else{
     this.toppings = 0
   }
-
-
-
+}
 $(document).ready(function() {
-  $("#submit").click(function() {
+  var userPizza = new Pizza ()
+  $("form#options").submit(function(event) {
     event.preventDefault();
     var userToppings1 = $("#toppings1").val();
     var userToppings2 = $("#toppings2").val();
     var userToppings3 = $("#toppings3").val();
     var userSize = $("#size").val();
-    $("#result").text(checkPrice)
+    $("#result").text(userPizza.checkPrice());
 
   });
 });

fabcc9ea984da5de53bcc64f1a5330a229158df0
Kyle Parsons <23gdsm@gmail.com>
2018-06-15 14:52:37 -0700

    add scripts fr pizza function

---
+++ README.md
@@ -0,0 +1,45 @@
+# _Pizza For Days Website_
+
+#### _Website built with JavaScript, bootstrap, and JQuery, 6/15/2018_
+
+#### By _**Kyle Parsons**_s
+
+## Description
+
+_A Pizza Parlor Website for Pizza pricing_
+
+## Setup/Installation Requirements
+
+* _computer_
+* _the internet_
+* _hands_
+* _power outlet_
+* _[Link to Kyle's repo] (https://github.com/owner5563/pizza)_
+
+
+_more instructions soon_
+
+## Known Bugs
+
+*_ _
+## Specs list
+1.Should have multiple pizza sizes
+2.Should have multiple toppings allowed for those sizes
+3.Should allow user to input size and toppings
+4.Should output price of pizza selected by customer
+
+
+
+
+## Support and contact details
+
+_Questions, Comments, or Support contact Kyle At 23gdsm@gmail.com
+## Technologies Used
+
+_HTML CSS JavaScript bootstrap JQuery_
+
+### License
+
+*Anyone who can wield the power of the internet may have access*
+
+Copyright (c) 2018 **_Kyle Enterprises_**
---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+function Pizza (size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.checkPrice = function () {
+return this.size + this.toppings;
+};
+
+
+Pizza.protoype.sizeSelect = function () {
+  if (userSize = "1") {
+    this.size = 8
+  }else if (userSize = "2") {
+    this.size = 10
+  }else  {
+    this.size = 12
+};
+
+Pizza.prototype.toppingsSelect= function () {
+
+  var toppingsTotal = userToppings1 + userToppings2 + userToppings3;
+  if (toppingsTotal = 1) {
+    this.toppings = 1.75
+  }else if (toppingsTotal = 2) {
+    this.toppings = 3.50
+  }else if (toppingsTotals = 3) {
+    this.toppings = 5.25
+  }else
+    this.toppings = 0
+  }
+
+
+
+$(document).ready(function() {
+  $("#submit").click(function() {
+    event.preventDefault();
+    var userToppings1 = $("#toppings1").val();
+    var userToppings2 = $("#toppings2").val();
+    var userToppings3 = $("#toppings3").val();
+    var userSize = $("#size").val();
+    $("#result").text(checkPrice)
+
+  });
+});
